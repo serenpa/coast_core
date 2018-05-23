d05820e7262b8db9e173bcecd5ccb386a5a79831
zedrem <ash.03@hotmail.com>
2018-05-23 20:39:18 +1200

    added version

--- coast_core/__init__.py
+++ coast_core/__init__.py
@@ -5,6 +5,13 @@
 
     Description:
 """
+from __future__ import print_function
+from ._version import get_versions
+
+__author__ = 'Ashley Williams'
+__email__ = 'ashley.williams@pg.canterbury.ac.nz'
+__version__ = get_versions()['version']
+del get_versions
 
 from coast_core import analyse_ngrams
 from coast_core import citations
