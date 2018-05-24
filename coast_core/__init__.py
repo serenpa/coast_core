ef7a1b6d65afa02695660c4cbfe69fbf73a3c4de
YannLeNorment <yann.lenorment@gmail.com>
2018-05-24 16:45:35 +1200

    Moved module imports to the rigth place

--- coast_core/__init__.py
+++ coast_core/__init__.py
@@ -8,11 +8,6 @@
 from __future__ import print_function
 from ._version import get_versions
 
-__author__ = 'Ashley Williams'
-__email__ = 'ashley.williams@pg.canterbury.ac.nz'
-__version__ = get_versions()['version']
-del get_versions
-
 from coast_core import analyse_ngrams
 from coast_core import citations
 from coast_core import clarity_of_writing
@@ -21,3 +16,8 @@ from coast_core import extraction
 from coast_core import markers
 from coast_core import named_entities
 from coast_core import utils
+
+__author__ = 'Ashley Williams'
+__email__ = 'ashley.williams@pg.canterbury.ac.nz'
+__version__ = get_versions()['version']
+del get_versions
