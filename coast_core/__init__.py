f67b9c0a5ee27849d7ce943d5b43b7106dfc1c61
YannLeNorment <yann.lenorment@gmail.com>
2018-05-28 16:56:13 +1200

    Added imports for each module in the init file

--- coast_core/__init__.py
+++ coast_core/__init__.py
@@ -8,6 +8,16 @@
 from __future__ import print_function
 from ._version import get_versions
 
+from coast_core import citations
+from coast_core import clarity_of_writing
+from coast_core import code_detection
+from coast_core import events
+from coast_core import extraction
+from coast_core import markers
+from coast_core import named_entities
+from coast_core import ngram_extraction
+from coast_core import utils
+
 __author__ = 'Ashley Williams'
 __email__ = 'ashley.williams@pg.canterbury.ac.nz'
 __version__ = get_versions()['version']
